bba3587f029e7135ab9b65b1db9118f3c3f1dc55
Cassio Jhones <56178855+CassioJhones@users.noreply.github.com>
2023-09-07 20:03:28 -0300

    Add files via upload

--- Cronometro/Cronometro/Program.cs
+++ Cronometro/Cronometro/Program.cs
@@ -2,10 +2,7 @@
 {
     private static void Main(string[] args)
     {
-        Start(1);
         Menu();
-
-
 
         void Menu()
         {
@@ -16,13 +13,35 @@
             Console.WriteLine("0 = Sair");
             Console.WriteLine("Quanto tempo quer contar? ");
 
-            string data = Console.ReadLine()!.ToLower();
-            char type = char.Parse(data.Substring(data.Length-1,1));
-            Console.WriteLine(data);
-            Console.WriteLine(type);
+            string data = Console.ReadLine()!.ToLower(); //tudo minusculo
+            char type = char.Parse(data.Substring(data.Length-1,1)); //converte em char e pega ultimo caracter digitado
+            int time = int.Parse(data.Substring(0,data.Length-1)); //converte em char e pega todos exceto o ultimo caracter digitado
+            int multiplier = 1;
+
+            if (type == 'm')
+                multiplier = 60;
             
+            if(time == 0) //sair
+            {
+                Console.Clear();
+                Console.WriteLine("\nFECHANDO APLICAÇÃO\n");
+                Environment.Exit(0);
+
+            }
+
+            PreStart(time * multiplier);
         }
 
+        void PreStart(int time)
+        {
+            Console.Clear();
+            Console.WriteLine("\nCarregando...");
+            Thread.Sleep(1400);
+            Console.Clear();
+            Console.WriteLine("\n---INICIADO---");
+            Thread.Sleep(1000);
+            Start(time);
+        }
 
 
         void Start(int time)
@@ -38,10 +57,10 @@
             }
 
             Console.Clear();
-            Console.WriteLine("Cronometro Finalizado");
+            Console.WriteLine("---Cronometro Finalizado---");
             Thread.Sleep(200);
+            Menu();
         }
-
         
     }
 }
